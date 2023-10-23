0e658051cf76f2ecdeeeecebc7ffddcee07a05aa
elenatrenzado <145114754+elenatrenzado@users.noreply.github.com>
2023-10-23 15:35:20 +0100

    added files

---
+++ BlueExercises/src/Bicycle.java
@@ -0,0 +1,10 @@
+public class Bicycle extends Vehicles {
+    int numberOfSeats;
+    String Pedals;
+    double seatHeight;
+
+    @Override
+    public void print_Colour() {
+        System.out.println("Not required");
+    }
+}
---
+++ BlueExercises/src/Vehicles.java
@@ -0,0 +1,10 @@
+public class Vehicles {
+    public static void main(String[] args) {
+        int Wheels;
+        String Colour;
+    }
+    public void print_Colour(){
+        System.out.println("green");
+    }
+}
+
