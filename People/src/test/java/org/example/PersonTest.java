1f714e943589a3781bad724d835c8ef742064b00
ddrgit <44521911+ddrgit@users.noreply.github.com>
2023-10-27 15:54:34 +0100

    Update PersonTest.java

--- People/src/test/java/org/example/PersonTest.java
+++ People/src/test/java/org/example/PersonTest.java
@@ -8,52 +8,52 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTest {
     @Test
-    void checkGetFirstName() {
+    void testGetFirstName() {
         Person person = new Person("John", "Smith", LocalDate.parse("2000-12-01"));
         assertEquals("John", person.getFirstName());
     }
 
     @Test
-    void checkSetFirstName() {
+    void testSetFirstName() {
         Person person = new Person("John", "Smith", LocalDate.parse("2000-12-01"));
         person.setFirstName("Peter");
         assertEquals("Peter", person.getFirstName());
     }
 
     @Test
-    void checkGetLastName() {
+    void testGetLastName() {
         Person person = new Person("John", "Smith", LocalDate.parse("2000-12-01"));
         assertEquals("Smith", person.getLastName());
     }
 
     @Test
-    void checkSetLastName() {
+    void testSetLastName() {
         Person person = new Person("John", "Smith", LocalDate.parse("2000-12-01"));
         person.setLastName("Williams");
         assertEquals("Williams", person.getLastName());
     }
 
     @Test
-    void checkGetDateOfBirth() {
+    void testGetDateOfBirth() {
         Person person = new Person("John", "Smith", LocalDate.parse("2000-12-01"));
         assertEquals(LocalDate.parse("2000-12-01"), person.getDateOfBirth());
     }
 
     @Test
-    void checkSetDateOfBirth() {
+    void testSetDateOfBirth() {
         Person person = new Person("John", "Smith", LocalDate.parse("2000-12-01"));
         person.setDateOfBirth(LocalDate.parse("2012-02-01"));
         assertEquals(LocalDate.parse("2012-02-01"), person.getDateOfBirth());
     }
 
     @Test
-    void checkGetAge() {
+    void testGetAge() {
         Person person = new Person("John", "Smith", LocalDate.now());
         assertEquals(0, person.getAge());
     }
 
     @Test
-    void checkGetInstances() {
+    void testGetInstances() {
         Person.instances = 0;
         Person person1 = new Person("John", "Smith", LocalDate.parse("2000-12-01"));
         Person person2 = new Person();
@@ -62,7 +62,7 @@ class PersonTest {
     }
 
     @Test
-    void checkToString() {
+    void testToString() {
         Person person = new Person("John", "Smith", LocalDate.parse("2000-12-01"));
         assertEquals("Name: John Smith\nDate of birth: 01-12-2000 (22 years old)", person.toString(LocalDate.parse("2023-10-27")));
     }
